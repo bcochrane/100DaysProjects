577a9992c4feaa6dac79b3f9a973e7968558f11c
Brian Cochrane <brian.j.cochrane@gmail.com>
2019-03-10 22:50:08 -0700

    specified character endcoding

--- Day4-6/directors_rating.py
+++ Day4-6/directors_rating.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('movie_metadata.csv') as csvfile:
+with open('movie_metadata.csv', newline='', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         print(row['director_name'], row['movie_title'], row['title_year'], row['imdb_score'])
